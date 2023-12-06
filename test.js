be47f778b4ab786ed90e9051c24d6103a8947bd7
Rosangel593 <148271323+Rosangel593@users.noreply.github.com>
2023-12-06 22:16:51 +0000

    dollar

---
+++ test.js
@@ -0,0 +1,8 @@
+
+test("One euro should be 1.07 dollars", function() {
+    const { fromEuroToDollar } = require('./app.js');
+    const dollars = fromEuroToDollar(3.5);
+    const expected = 3.5 * 1.07;
+    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+})
+
